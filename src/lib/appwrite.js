8a97911c9f74379078659b5634769be9b46204b6
tonybnya <nya.tony2010@gmail.com>
2024-06-12 18:07:32 +0100

    feat: configure the database

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,7 +1,12 @@
-import { Client } from "appwrite";
+import { Client, Databases } from "appwrite";
 
 const client = new Client();
+const DB_ID = "6669d38b003569881123";
+const COLLECTION_ID = "6669d3bd000b9f106307";
 
 client
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject("6669d1db00086ecaaffa");
+
+export const databases = new Databases();
+export { DB_ID, COLLECTION_ID };
